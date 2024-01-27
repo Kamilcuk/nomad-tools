66c4134d73914c7a55f4c90d5ad4a863a6a7bae7
Kamil Cukrowski <kamilcukrowski@gmail.com>
2024-01-27 14:16:46 +0100

    unit_test.sh: mask nomad executable for the duration of unit tests

--- unit_tests.sh
+++ unit_tests.sh
@@ -3,8 +3,15 @@ r() {
 	echo "+ $*" >&2
 	time "$@"
 }
-uset NOMAD_TOKEN
+# No connection to Nomad for unit tests is allowed.
+unset NOMAD_TOKEN
 if [[ -v NOMAD_ADDR ]]; then
 	NOMAD_ADDR=moon
 fi
+# Remove nomad executable from path completely.
+tmpd=$(mktemp -d)
+trap 'rm -rf "$tmpd"' EXIT
+printf "%s\n" "#!/bin/sh" "exit 1" >"$tmpd"/nomad
+chmod +x "$tmpd"/nomad
+export PATH="$tmpd:$PATH"
 r pytest -sxv tests/unit "$@"
