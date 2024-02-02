90e96296901ff3a3a9bb6d8050f8a4298995308d
Kamil Cukrowski <kamilcukrowski@gmail.com>
2024-02-02 21:17:17 +0100

    watch: if a output pipeline is closed, just exit with 1 to handle grep properly

--- src/nomad_tools/nomad_watch.py
+++ src/nomad_tools/nomad_watch.py
@@ -323,7 +323,14 @@ class Mylogger:
         except KeyError:
             log.exception(f"fmt={ARGS.log_format!r} params={ARGS}")
             raise
-        print(out, flush=True)
+        try:
+            print(out, flush=True)
+        except BrokenPipeError:
+            # Python flushes standard streams on exit; redirect remaining output
+            # to devnull to avoid another BrokenPipeError at shutdown
+            devnull = os.open(os.devnull, os.O_WRONLY)
+            os.dup2(devnull, sys.stdout.fileno())
+            sys.exit(1)  # Python exits with error code 1 on EPIPE
 
     @classmethod
     def log_eval(cls, db: NomadDbJob, eval: nomadlib.Eval, message: str):
