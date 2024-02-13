fc54acc3aaf7e1e9b1e0031e303090ac81abd448
Kamil Cukrowski <kamilcukrowski@gmail.com>
2024-02-13 20:36:28 +0100

    tests/localnomad add for testing noamd version

---
+++ tests/localnomad.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import subprocess
+from pathlib import Path
+from shlex import quote, split
+from typing import List
+
+parser = argparse.ArgumentParser()
+parser.add_argument("version", nargs="?")
+args = parser.parse_args()
+DIR = Path(__file__).parent
+os.chdir(DIR)
+
+if not args.version:
+    cmdstr: str = "nomad-downloadrelease --showversion nomad"
+    cmd: List[str] = split(cmdstr)
+    print(f"+ {cmdstr}")
+    args.version = subprocess.check_output(cmd, text=True).strip()
+
+exe = DIR / "build" / f"nomad{args.version}"
+if not exe.exists():
+    cmdstr = f"nomad-downloadrelease -p {quote(args.version)} nomad {quote(str(exe))}"
+    cmd = split(cmdstr)
+    print(f"+ {cmdstr}")
+    subprocess.check_call(cmd)
+
+cmd = [
+    *(["sudo"] if os.getuid() != 0 else []),
+    str(exe),
+    *"agent -dev -config ./nomad.hcl -log-level debug".split(),
+]
+cmdstr = " ".join(quote(x) for x in cmd)
+print(f"+ {cmdstr}")
+os.execvp(cmd[0], cmd)
