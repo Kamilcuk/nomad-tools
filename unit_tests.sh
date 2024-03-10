a4939d0bc3cbc96fb2a9eba634e9cebd59a1cfc7
Kamil Cukrowski <kamilcukrowski@gmail.com>
2024-03-10 21:43:17 +0100

    unit_tests: check for absolute includes that should be relative

--- unit_tests.sh
+++ unit_tests.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 cd "$(dirname "$(readlink -f "$0")")"
+
+fatal() {
+	echo "$@" >&2
+	exit 123
+}
+
 r() {
 	echo "+ $*" >&2
 	"$@"
 }
+
 cleanuplogs() {
 	rm -f build/tests/*.log
 }
+
 logs_errors() {
 	local files gfiles
 	files=(build/tests/*.log)
@@ -23,6 +31,7 @@ logs_errors() {
 	# shellcheck disable=2086
 	tail -n +1 $gfiles || :
 }
+
 remove_nomad_from_path() {
 	# Remove nomad executable from path completely.
 	bind="$PWD"/build/bin
@@ -36,30 +45,35 @@ remove_nomad_from_path() {
 	unset NOMAD_TOKEN
 	export NOMAD_ADDR=moon
 }
+
 unit_tests_trap_exit() {
 	logs_errors || :
 }
+
 unit_tests() {
 	# No connection to Nomad for unit tests is allowed.
 	remove_nomad_from_path
 	r pytest -sxv tests/unit "$@"
 }
+
 integration_tests() {
 	trap 'integration_tests_trap_exit' exit
 	r pytest -sxv tests/unit tests/integration "$@"
 }
+
 integration_tests_trap_exit() {
 	logs_errors || :
 	nomad status || :
 	docker ps || :
 }
+
 be_nice() {
 	if [[ -v CI ]]; then
 		# Ignore in CI/CD
 		return
 	fi
-	r renice -n 40 $BASHPID
-	r ionice -c 3 -p $BASHPID
+	renice -n 40 $BASHPID >/dev/null
+	ionice -c 3 -p $BASHPID >/dev/null
 }
 
 ###############################################################################
@@ -69,5 +83,8 @@ be_nice
 cleanuplogs
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
 	# If not sourced, execute unit tests
+	if r grep -E 'import.*nomad_tools|nomad_tools.*import' --exclude-dir __pycache__ -R src/nomad_tools/; then
+		fatal "You have non-relative includes in source dir"
+	fi
 	unit_tests "$@"
 fi
