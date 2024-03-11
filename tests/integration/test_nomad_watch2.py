6f1a3649c53da6fbce8c2259da5110865835471a
Kamil Cukrowski <kamilcukrowski@gmail.com>
2024-03-12 00:41:46 +0100

    tests: try to increase time

--- tests/integration/test_nomad_watch2.py
+++ tests/integration/test_nomad_watch2.py
@@ -112,7 +112,7 @@ def test_nomad_watch2_blocked():
             for j in range(2):
                 run_nomad_watch(
                     f"{i} -var block=true {testjobs[job]}",
-                    timeout=10,
+                    timeout=12,
                     check=124,
                     output=[
                         "Placement Failures",
