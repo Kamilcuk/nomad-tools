128929ed41a48826fe4bbb352d415ace31b46011
Kamil Cukrowski <kamilcukrowski@gmail.com>
2024-07-07 00:12:03 +0200

    fix and test TLS connection

--- tests/tls_env.bash
+++ tests/tls_env.bash
@@ -4,47 +4,68 @@ _r() {
 	echo "+ $*" >&2
 	"$@"
 }
-case "$1" in
-clear)
-	_r unset NOMAD_ADDR
-	_r unset NOMAD_TLS_SERVER_NAME
-	_r unset NOMAD_CACERT
-	_r unset NOMAD_CAPATH
-	_r unset NOMAD_CLIENT_CERT
-	_r unset NOMAD_CLIENT_KEY
-	;;
-tls)
-	_r export NOMAD_ADDR=https://localhost:4646
-	_r unset NOMAD_TLS_SERVER_NAME
-	_r export NOMAD_CACERT=$_DIR/nomad-agent-ca.pem
-	_r unset NOMAD_CAPATH
-	_r export NOMAD_CLIENT_CERT=$_DIR/global-cli-nomad.pem
-	_r export NOMAD_CLIENT_KEY=$_DIR/global-cli-nomad-key.pem
-	;;
-capath)
-	_r export NOMAD_ADDR=https://localhost:4646
-	_r unset NOMAD_TLS_SERVER_NAME
-	_r unset NOMAD_CACERT
-	_r export NOMAD_CAPATH=$_DIR/capath
-	_r export NOMAD_CLIENT_CERT=$_DIR/global-cli-nomad.pem
-	_r export NOMAD_CLIENT_KEY=$_DIR/global-cli-nomad-key.pem
-	;;
-sni)
-	_r export NOMAD_ADDR=https://127.0.0.1:4646
-	_r export NOMAD_TLS_SERVER_NAME=localhost
-	_r export NOMAD_CACERT=$_DIR/nomad-agent-ca.pem
-	_r unset NOMAD_CAPATH
-	_r export NOMAD_CLIENT_CERT=$_DIR/global-cli-nomad.pem
-	_r export NOMAD_CLIENT_KEY=$_DIR/global-cli-nomad-key.pem
-	;;
-test)
-	_r nomad status
-	_r nomadtools vardir -j nginx@nginx ls
-	;;
-*)
-	echo "${BASH_SOURCE[0]}: invalid argument: $1" >&2
-	echo "${BASH_SOURCE[0]}: must be: clear tls sni" >&2
-	;;
-esac
-unset -f _r
-unset _DIR
+_clear_envs() {
+	envs=$(compgen -v NOMAD_)
+	if [[ -n "$envs" ]]; then
+		# shellcheck disable=2086
+		_r unset $envs
+	fi
+}
+while (($#)); do
+	case "$1" in
+	clear)
+		_clear_envs
+		;;
+	tls)
+		_clear_envs
+		_r export NOMAD_ADDR=https://localhost:4646
+		_r unset NOMAD_TLS_SERVER_NAME
+		_r export NOMAD_CACERT="$_DIR"/nomad-agent-ca.pem
+		_r unset NOMAD_CAPATH
+		_r export NOMAD_CLIENT_CERT="$_DIR"/global-cli-nomad.pem
+		_r export NOMAD_CLIENT_KEY="$_DIR"/global-cli-nomad-key.pem
+		;;
+	capath)
+		_clear_envs
+		_r export NOMAD_ADDR=https://localhost:4646
+		_r unset NOMAD_TLS_SERVER_NAME
+		_r unset NOMAD_CACERT
+		_r export NOMAD_CAPATH="$_DIR"/capath
+		_r export NOMAD_CLIENT_CERT="$_DIR"/global-cli-nomad.pem
+		_r export NOMAD_CLIENT_KEY="$_DIR"/global-cli-nomad-key.pem
+		;;
+	sni)
+		_clear_envs
+		_r export NOMAD_ADDR=https://127.0.0.1:4646
+		_r export NOMAD_TLS_SERVER_NAME=localhost
+		_r export NOMAD_CACERT="$_DIR"/nomad-agent-ca.pem
+		_r unset NOMAD_CAPATH
+		_r export NOMAD_CLIENT_CERT="$_DIR"/global-cli-nomad.pem
+		_r export NOMAD_CLIENT_KEY="$_DIR"/global-cli-nomad-key.pem
+		;;
+	test)
+		_r export NOMAD_NAMESPACE=default
+		_r nomad status
+		_r nomadtools vardir -j nginx@nginx ls
+		_r python -m nomad_tools.taskexec test-forever test-forever echo Hello world
+		;;
+	testall)
+		_r "$0" tls -- nomadtools watch start "$_DIR"/../../jobs/test-forever.nomad.hcl
+		echo
+		_r "$0" tls test
+		echo
+		_r "$0" capath test
+		echo
+		_r "$0" sni test
+		;;
+	--)
+		shift
+		_r exec "$@"
+		;;
+	*)
+		echo "Unknown arguments: $*" >&2
+		exit 123
+		;;
+	esac
+	shift
+done
