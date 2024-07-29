553d7cec7c858bb11666ebbcf37033741623e5b2
Kamil Cukrowski <kamilcukrowski@gmail.com>
2024-07-29 23:39:04 +0200

    tests: downloadrelease unit tests are too slow, just check version

--- tests/unit/test_downloadrelease.py
+++ tests/unit/test_downloadrelease.py
@@ -6,7 +6,11 @@ import pytest
 from tests.testlib import run_downloadrelease
 
 
-@pytest.mark.skip()
+def versiontuple(v):
+    return tuple(map(int, (v.split("."))))
+
+
+@pytest.mark.skip()  # too slow
 @pytest.mark.parametrize("tool", "nomad consul".split())
 def test_downloadrelease(tool):
     with tempfile.TemporaryDirectory() as d:
@@ -14,3 +18,10 @@ def test_downloadrelease(tool):
         toolf = f"{d}/{tool}"
         assert os.path.exists(toolf)
         os.remove(toolf)
+
+
+@pytest.mark.parametrize("tool", "nomad consul".split())
+def test_downloadrelease_version(tool):
+    rr = run_downloadrelease(f"--showversion {tool}", stdout=True)
+    assert len(versiontuple(rr.stdout)) == 3
+    assert versiontuple(rr.stdout) > versiontuple("1.8.0")
