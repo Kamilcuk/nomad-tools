183e5eca84519c81014b190aaba75d701da2c9a9
Kamil Cukrowski <kamilcukrowski@gmail.com>
2024-07-29 23:29:50 +0200

    cli: allow shortening commands like nomadtools w r

---
+++ src/nomad_tools/aliasedgroup.py
@@ -0,0 +1,21 @@
+# https://click.palletsprojects.com/en/8.1.x/advanced/
+import click
+
+
+class AliasedGroup(click.Group):
+    def get_command(self, ctx, cmd_name):
+        rv = click.Group.get_command(self, ctx, cmd_name)
+        if rv is not None:
+            return rv
+        matches = [x for x in self.list_commands(ctx) if x.startswith(cmd_name)]
+        if not matches:
+            return None
+        elif len(matches) == 1:
+            return click.Group.get_command(self, ctx, matches[0])
+        ctx.fail(f"Too many matches: {', '.join(sorted(matches))}")
+
+    def resolve_command(self, ctx, args):
+        # always return the full command name
+        _, cmd, args = super().resolve_command(ctx, args)
+        assert cmd
+        return cmd.name, cmd, args
