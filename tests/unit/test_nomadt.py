85c9e9b06a8c4e1ee79563df889d5364256a40f4
Kamil Cukrowski <kamilcukrowski@gmail.com>
2024-07-30 13:19:21 +0200

    tests: add more unit tests

--- tests/unit/test_nomadt.py
+++ tests/unit/test_nomadt.py
@@ -1,9 +1,23 @@
+import pytest
+import yaml
+
 from tests.testlib import run_nomadt
 
 
 def test_nomadt_help():
     run_nomadt("-h")
     run_nomadt("--help")
-    run_nomadt("watch --help")
-    run_nomadt("port --help")
-    run_nomadt("cp --help")
+
+
+@pytest.mark.parametrize(
+    "app",
+    "constrainteval cp dockers downloadrelease gitlab-runner".split()
+    + "githubrunner go port vardir watch".split(),
+)
+def test_nomadt_app_help(app):
+    run_nomadt(f"{app} --help")
+
+
+def test_githubrunner_dumpconfig():
+    rr = run_nomadt("githubrunner -c '---\nrepos: [test]\nloop: 123' dumpconfig", stdout=True)
+    assert yaml.safe_load(rr.stdout)
